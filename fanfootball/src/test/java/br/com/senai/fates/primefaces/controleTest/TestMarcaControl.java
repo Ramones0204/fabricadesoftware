870699075f5bbd81d22c28375aa8ff6d290f04c7
aluno <aluno@LAB104-XX.nox5.lan>
2019-05-15 19:41:49 -0300

    Testes nao Implementados do Controle

---
+++ fanfootball/src/test/java/br/com/senai/fates/primefaces/controleTest/TestMarcaControl.java
@@ -0,0 +1,29 @@
+package br.com.senai.fates.primefaces.controleTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMarcaControl {
+
+	@Test
+	public void testConfirmar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExcluir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelecionarMarcas() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ fanfootball/src/test/java/br/com/senai/fates/primefaces/controleTest/TestTimeControl.java
@@ -0,0 +1,34 @@
+package br.com.senai.fates.primefaces.controleTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTimeControl {
+
+	@Test
+	public void testConfirmar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListarLiga() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExcluir() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelecionarTimes() {
+		fail("Not yet implemented");
+	}
+
+}
